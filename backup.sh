50604077c203fbf2cc8e432a658704d8b24a4135
Bhargav Lad <ladbhargav97@gmail.com>
2021-05-06 17:18:40 -0400

    added backup script

---
+++ backup.sh
@@ -0,0 +1,8 @@
+[ ! -d "./hammerspoon" ] && mkdir hammerspoon
+[ ! -d "./nvim" ] && mkdir nvim
+[ ! -d "./tmux" ] && mkdir "tmux"
+[ ! -d "./zsh" ] && mkdir "zsh"
+# rsync -r ~/.hammerspoon/ hammerspoon
+# rsync -r ~/.config/nvim/ nvim
+# rsync ~/.tmux.conf tmux/tmux.conf
+# rsync ~/.zshrc zsh/zshrc
